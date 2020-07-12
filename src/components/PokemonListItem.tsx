bbf7dcfc38d348220733c72f095d379bf732ef46
tomholford <tomholford@users.noreply.github.com>
2020-07-12 14:12:18 +0900

    some color mappings

--- src/components/PokemonListItem.tsx
+++ src/components/PokemonListItem.tsx
@@ -6,18 +6,22 @@ import PokemonTypeBadge from './PokemonTypeBadge';
 const PokemonListItem: React.FC<{ pokemon: Pokemon }> = ({ pokemon }) => {
   return (
     <div className="box">
-      <article className="level">
-        <figure className="level-item">
-          <p className="image is-64x64">
-            {/* <img src={pokemon.spriteURL} alt=""/> */}
-          </p>
-        </figure>
-        <div className="level-item">
+      <article className="columns is-vcentered">
+        <div className="column">
           <div className="content">
-            <p className="subtitle displayname">{pokemon.displayName}</p>
+            <figure className="poke-image">
+              <p className="image is-64x64">
+                <img src={pokemon.spriteURL} alt=""/>
+              </p>
+            </figure>
           </div>
         </div>
-        <div className="level-item">
+        <div className="column">
+          <div className="content">
+            <p className="subtitle is-capitalized has-text-centered">{pokemon.displayName}</p>
+          </div>
+        </div>
+        <div className="column">
           <div className="content">
             <div className="tags are-large">
               { pokemon.types?.map((type: PokemonType) => <PokemonTypeBadge key={type.key} type={type} />) }
--- src/components/PokemonTypeBadge.tsx
+++ src/components/PokemonTypeBadge.tsx
@@ -2,8 +2,33 @@ import React from 'react';
 import PokemonType from '../models/pokemon_type';
 
 const PokemonTypeBadge: React.FC<{ type: PokemonType }> = ({ type }) => {
+  const badgeColor = (name: string) => {
+    switch(name) {
+      case 'fire':
+        return 'is-danger';
+      case 'water':
+        return 'is-link';
+      case 'grass':
+        return 'is-success';
+      case 'poison':
+        return 'is-primary';
+      case 'normal':
+        return 'is-light';
+      case 'steel':
+          return 'is-light';
+      case 'electric':
+        return 'is-warning';
+      case 'ghost':
+        return 'is-white';
+      case 'flying':
+        return 'is-light';
+      default:
+        return 'is-light';
+    }
+  }
+
   return (
-    <span className="tag is-link is-light">{type.name}</span>
+    <span className={['tag', 'is-light', badgeColor(type.name)].join(' ')}>{type.name}</span>
   );
 }
 
