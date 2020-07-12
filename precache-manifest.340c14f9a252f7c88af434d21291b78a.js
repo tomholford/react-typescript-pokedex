a3072463b6353348c5bf7d4c02d020f8df1013f9
tomholford <tomholford@users.noreply.github.com>
2020-07-12 14:18:22 +0900

    Updates

---
+++ precache-manifest.340c14f9a252f7c88af434d21291b78a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16395d608e0c098646d90a626eeac1c6",
+    "url": "/react-typescript-pokedex/index.html"
+  },
+  {
+    "revision": "f6c273d4ea097187b5d2",
+    "url": "/react-typescript-pokedex/static/css/main.b5f3e29b.chunk.css"
+  },
+  {
+    "revision": "2bd9bbc7a140fcb4b8cb",
+    "url": "/react-typescript-pokedex/static/js/2.3b6d6b1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-typescript-pokedex/static/js/2.3b6d6b1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c273d4ea097187b5d2",
+    "url": "/react-typescript-pokedex/static/js/main.4c7bae20.chunk.js"
+  },
+  {
+    "revision": "85d44b83508dcced7535",
+    "url": "/react-typescript-pokedex/static/js/runtime-main.5bf5dbc8.js"
+  }
+]);
